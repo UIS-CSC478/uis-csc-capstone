2dd55f80347856c0526b2902886f7d4a3ed13d5e
Wendy Stump <stump.wendy@yahoo.com>
2026-02-04 10:52:02 -0500

    Update Main.java
    Added JavaFX call

--- src/main/java/csc478/group2/Main.java
+++ src/main/java/csc478/group2/Main.java
@@ -1,10 +1,7 @@
 package csc478.group2;
 
 public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("Hello World");
-	}
-
+    public static void main(String[] args) {
+        JavaFX.launch(JavaFX.class, args);
+    }
 }
